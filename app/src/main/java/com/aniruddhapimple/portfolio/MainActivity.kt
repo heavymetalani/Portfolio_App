c867cc33b1050409c56b26192c19bc492ea125b4
heavymetalani <apimple@stevens.edu>
2019-08-16 21:34:10 -0400

    Tentatively completed home page

--- app/src/main/java/com/aniruddhapimple/portfolio/MainActivity.kt
+++ app/src/main/java/com/aniruddhapimple/portfolio/MainActivity.kt
@@ -2,11 +2,18 @@ package com.aniruddhapimple.portfolio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.PicassoProvider
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
+
+    val dp_url = "https://firebasestorage.googleapis.com/v0/b/aniruddhapimpleportfolio.appspot.com/o/pictures%2FStevens%20LinkedIn%20Headshots%20February%202019edited.jpg?alt=media&token=f535901d-a869-401c-9b40-42f60a4fa147"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        Picasso.get().load(dp_url).into(home_screen_picture)
     }
 }
