942a0343aa60182d017a81597de43f57f726fe2d
heavymetalani <apimple@stevens.edu>
2019-08-18 19:26:41 -0400

    Added activity animation

--- app/src/main/java/com/aniruddhapimple/portfolio/AboutAppActivity.kt
+++ app/src/main/java/com/aniruddhapimple/portfolio/AboutAppActivity.kt
@@ -2,10 +2,12 @@ package com.aniruddhapimple.portfolio
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.about_app_activity.*
 
 class AboutAppActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.about_app_activity)
+        back_button_about_activity.setOnClickListener{finish()}
     }
 }
