bda6141d2be1ec3c32785947621d55e1b5a49557
heavymetalani <apimple@stevens.edu>
2019-08-19 13:31:49 -0400

    youtube video working

--- app/src/main/java/com/aniruddhapimple/portfolio/ActivitiesActivity.kt
+++ app/src/main/java/com/aniruddhapimple/portfolio/ActivitiesActivity.kt
@@ -1,6 +1,9 @@
 package com.aniruddhapimple.portfolio
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activities_activity.*
@@ -19,5 +22,14 @@ class ActivitiesActivity : AppCompatActivity() {
     private fun retrieveList(): ArrayList<ListableItem> {
         return arrayListOf(WorkDataObject("CMO","Google",
             "Jun 96 - Aug 19","lavdu lasun\nlasun lavda\n aur kya chahiye\n punnja \n maa ki aankh") as ListableItem)
+    }
+
+    fun onVideoDemoClick(view : View){
+        val intent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("https://youtu.be/Ke3PyAUkZFE")
+        )
+        intent.putExtra("force_fullscreen", true)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/aniruddhapimple/portfolio/MainActivity.kt
+++ app/src/main/java/com/aniruddhapimple/portfolio/MainActivity.kt
@@ -8,30 +8,32 @@ import android.view.View
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.main_activity.*
 
-class MainActivity : AppCompatActivity()  {
+class MainActivity : AppCompatActivity() {
 
-    private val dpUrl = "https://firebasestorage.googleapis.com/v0/b/aniruddhapimpleportfolio.appspot.com/o/pictures%2FStevens%20LinkedIn%20Headshots%20February%202019edited.jpg?alt=media&token=f535901d-a869-401c-9b40-42f60a4fa147"
+    private val dpUrl =
+        "https://firebasestorage.googleapis.com/v0/b/aniruddhapimpleportfolio.appspot.com" +
+                "/o/pictures%2FStevens%20LinkedIn%20Headshots%20February%202019edited.jpg?alt=" +
+                "media&token=f535901d-a869-401c-9b40-42f60a4fa147"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
         Picasso.get().load(dpUrl).into(home_screen_picture)
-
     }
 
-    fun onWorkExProjectsClick(view : View){
+    fun onWorkExProjectsClick(view: View) {
         val intent = Intent(this, WorkProjectEducationActivity::class.java)
         when {
-            view.id == R.id.work_ex_layout -> intent.putExtra("activity_title","Work Experience")
-            view.id == R.id.project_layout -> intent.putExtra("activity_title","Projects")
-            else -> intent.putExtra("activity_title","Education")
+            view.id == R.id.work_ex_layout -> intent.putExtra("activity_title", "Work Experience")
+            view.id == R.id.project_layout -> intent.putExtra("activity_title", "Projects")
+            else -> intent.putExtra("activity_title", "Education")
         }
         startActivity(intent)
     }
 
-    fun onWebLinkClick(view : View){
-        var url : String = ""
+    fun onWebLinkClick(view: View) {
+        var url = ""
         when {
             view.id == R.id.linkedin_link_text_view -> url = resources.getString(R.string.linkedin_link)
             view.id == R.id.github_link_text_view -> url = resources.getString(R.string.github_link)
@@ -41,29 +43,34 @@ class MainActivity : AppCompatActivity()  {
         startActivity(intent)
     }
 
-    fun onAboutAppClick(view : View){
+    fun onAboutAppClick(view : View) {
         val intent = Intent(this, AboutAppActivity::class.java)
         startActivity(intent)
     }
 
-    fun onSkillsClick(view : View){
+    fun onSkillsClick(view : View) {
         val intent = Intent(this, SkillsActivity::class.java)
         startActivity(intent)
     }
 
-    fun onActivitiesClick(view: View){
+    fun onActivitiesClick(view : View) {
         val intent = Intent(this, ActivitiesActivity::class.java)
         startActivity(intent)
     }
 
-    fun onEmailClick(view: View){
-        val intent = Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-            "mailto",resources.getString(R.string.email_link), null))
+    fun onEmailClick(view : View) {
+        val intent = Intent(
+            Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", resources.getString(R.string.email_link), null
+            )
+        )
         startActivity(Intent.createChooser(intent, "Send email..."))
     }
 
-    fun onPhoneNumberClick(view: View){
-        val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", resources.getString(R.string.phone_number), null))
+    fun onPhoneNumberClick(view : View) {
+        val intent = Intent(
+            Intent.ACTION_DIAL, Uri.fromParts(
+                "tel", resources.getString(R.string.phone_number), null))
         startActivity(intent)
     }
 }
