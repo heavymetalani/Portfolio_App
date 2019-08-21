8937bec96d791a46bb5efb28657a5145460c7ae0
heavymetalani <apimple@stevens.edu>
2019-08-20 22:27:06 -0400

    added FCM

---
+++ app/src/main/java/com/aniruddhapimple/portfolio/MyFirebaseMessagingService.kt
@@ -0,0 +1,65 @@
+package com.aniruddhapimple.portfolio
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+open class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onDeletedMessages() {
+        super.onDeletedMessages()
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        val title = remoteMessage.notification?.title
+        val body = remoteMessage.notification?.body
+        Log.d("FirebaseMesService",title+body)
+        sendNotification(title, body)
+    }
+
+    private fun sendNotification(title: String?, body: String?) {
+        val notificationManager : NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel =
+                NotificationChannel("my_channel_id", "My Notifications", NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.description = "Channel description"
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.vibrationPattern = longArrayOf(0, 1000, 500, 1000)
+            notificationChannel.enableVibration(true)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0,
+            intent, PendingIntent.FLAG_ONE_SHOT
+        )
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, "my_channel_id")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setContentInfo("Info")
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+}
